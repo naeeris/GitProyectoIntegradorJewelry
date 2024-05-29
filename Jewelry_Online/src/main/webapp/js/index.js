1ddd41de6f991afab4d88ee61250f21521deaef1
naeeris <noeliacauqui25@gmail.com>
2024-05-29 19:23:09 +0200

    Diseño index hecho

---
+++ Jewelry_Online/src/main/webapp/js/index.js
@@ -0,0 +1,17 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+	
+	let searchIcon = document.getElementById('icono-buscar');
+    let searchBar = document.getElementById('barra-buscar');
+	
+	searchIcon.addEventListener('click', function(){
+		
+		searchBar.classList.toggle('hidden');
+        searchBar.querySelector('input').focus();  // Pone el foco en el input cuando se muestra
+		
+	});
+	
+});
+  
+
+ 
